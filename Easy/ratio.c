f5962a0be9aeefeefb6bd8452ba529d22d74a4c1
Travis Kerr <kerrta@users.noreply.github.com>
2017-11-17 20:14:34 -0500

    Commented ratio.c

--- Easy/ratio.c
+++ Easy/ratio.c
@@ -9,19 +9,24 @@
 int main(){
     int n;
     double pos = 0, neg = 0, zero = 0; 
+    // Gets the number of inputs in the array
     scanf("%d",&n);
     int arr[n];
+    // Scans in the inputs into an array
     for(int arr_i = 0; arr_i < n; arr_i++){
        scanf("%d",&arr[arr_i]);
-       
+       // Count negative numbers
         if(arr[arr_i] < 0)
             neg += 1;
+        // Count positive numbers
         else if(arr[arr_i] > 0)
             pos += 1;
+        // Count zeros
         else
             zero += 1;
     }
     
+    // Prints the ratios
     printf("%f\n", (pos/n));
     printf("%f\n", (neg/n));
     printf("%f\n", (zero/n));
